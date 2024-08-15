213954e40ce212be8ecaf5d5617ef914ff30a033
林信欢 <2021469084@qq.com>
2024-08-15 15:42:42 +0800

    add:TrayTimeTask.java

---
+++ src/main/java/com/sinvon/traywords/task/TrayTimeTask.java
@@ -0,0 +1,28 @@
+package com.sinvon.traywords.task;
+
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.RandomUtil;
+import com.sinvon.traywords.common.SysConstant;
+import com.sinvon.traywords.utils.BaiCiZhanAPIUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+/**
+ * 定时任务
+ *
+ * @author sinvon
+ * @since 2024年8月14日19:21:38
+ */
+
+@Slf4j
+@Component
+public class TrayTimeTask {
+}
